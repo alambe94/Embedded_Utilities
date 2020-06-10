0a9fdbf74400a53ba7bb68cd0ce9eec2b6f3cb2c
unknown <you@example.comlambealsaad@gmail.com>
2020-06-10 21:09:09 +0530

    disclaimer

--- Rotary_Encoder/rotary_encoder.c
+++ Rotary_Encoder/rotary_encoder.c
@@ -1,10 +1,31 @@
-#include "rotary_encoder.h"
-#include "stddef.h"
-
-/** based on
+/**
  *  multi encoder code with speed up values adapted for STM32-arduino by Matthias Diro
  *  http://forums.leaflabs.com/topic.php?id=1107
- **/
+ */
+/*
+ * file version V0.0.0
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include "rotary_encoder.h"
+#include "stddef.h"
 
 /**
  * @brief assert implemenation, set USE_ENCODER_ASSERT to 1 to enable assert
